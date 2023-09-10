f776205c0e805ead4e42252d200ed497bbfaaed2
Pablo Guala <gualapab@gmail.com>
2023-09-10 18:35:57 -0300

    v0

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//Al cargar contenido DOM
 document.addEventListener("DOMContentLoaded", () => {
-  var boton = document.getElementById("submit");
+  //Botón
+  let boton = document.getElementById("submit");
+  let rs = document.getElementById("rs");
+  //En click
   boton.addEventListener("click", (evento) => {
     evento.preventDefault();
-    var nam = document.getElementById("name");
-    var srn = document.getElementById("surname");
-    var dob = document.getElementById("dob");
-    var rs = document.getElementById("rs");
 
-    console.log("Datos:" + nam.value + " " + srn.value + " " + dob.value);
-    var data =
+    //Valores
+    let nam = document.getElementById("name");
+    let srn = document.getElementById("surname");
+    let dob = document.getElementById("dob");
+    let data =
       "{nombre: " +
       nam.value +
       ", apellido: " +
@@ -16,23 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
       ", nacimiento: " +
       dob.value +
       ",}";
-    console.log("String:" + data);
+
+    console.log(
+      "Datos ingresados: " + nam.value + " " + srn.value + " " + dob.value
+    );
+    console.log("String generado: " + data);
     fetch("https://jsonplaceholder.typicode.com/users", {
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
       method: "POST",
-      body: JSON.stringify({
-        title: nam,
-        body: srn,
-        userId: dob,
-      }),
+      body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((json) => {
         alert("Enviado");
         console.log(json);
-        rs.innerHTML= '<div class="alert alert-success">Enviado!</div><br><p>'+ JSON.stringify(json)+'</p>';
+        rs.innerHTML =+
+          '<div class="alert alert-success">Enviado!</div><br><div class="alert alert-secondary">' +
+          JSON.stringify(json) +
+          "</div>";
       });
   });
 });
