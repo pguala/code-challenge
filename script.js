32b7870705ede3dece1451c334dc051f3f0bbbc6
Pablo Guala <gualapab@gmail.com>
2023-09-10 17:33:36 -0300

    v0

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-var boton = document.getElementById("submit");
-
-boton.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var nam = document.getElementById("name").value;
-    var srn = document.getElementById("surname").value;
-    var dob = document.getElementById("dob").value;
+document.addEventListener("DOMContentLoaded", () => {
+  var boton = document.getElementById("submit");
+  boton.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    var nam = document.getElementById("name");
+    var srn = document.getElementById("surname");
+    var dob = document.getElementById("dob");
     var rs = document.getElementById("rs");
 
-    console.log("Datos:" + nam + " " + srn + " " + dob);
-    var data = "{nombre: " + nam + ", apellido: " + srn + ", nacimiento: " + dob + ",}";
+    console.log("Datos:" + nam.value + " " + srn.value + " " + dob.value);
+    var data =
+      "{nombre: " +
+      nam.value +
+      ", apellido: " +
+      srn.value +
+      ", nacimiento: " +
+      dob.value +
+      ",}";
     console.log("String:" + data);
-
     fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "POST",
-        body: JSON.stringify({
-            title: nam,
-            body: srn,
-            userId: dob,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        title: nam,
+        body: srn,
+        userId: dob,
+      }),
     })
-        .then((response) => response.json())
-        .then((json) => function () {
-            rs.innerHTML(json);
-            console.log(json);
-        }
-        );
-}
-);
-
-
+      .then((response) => response.json())
+      .then((json) => {
+        alert("Enviado");
+        console.log(json);
+      });
+  });
+});
