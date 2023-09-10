e5c83915c17c5c97addcf9f9a9e6376febe0812d
Pablo Guala <gualapab@gmail.com>
2023-09-10 20:23:19 -0300

    v2

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
         //alert("Enviado");
         console.log(json);
         rs.innerHTML =
-          '<div class="alert alert-success">Enviado</div><br><div class="alert alert-secondary"><b>Consola ≥</b> '+JSON.stringify(json)+'</div>';
+          '<div class="alert alert-success"><b class="v">✔</b> Enviado</div><br><div class="alert alert-secondary"><b class="g">Consola ≥</b> '+JSON.stringify(json)+'</div>';
       });
   });
 });
